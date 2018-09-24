1547d0647aaa928477089b23f2c78d039d5254b6
Andrew Lau <andrewlau@Andrews-MacBook-Pro-3.local>
2018-09-24 14:12:25 -0700

    txt file import

--- createTable.java
+++ createTable.java
@@ -1,22 +1,38 @@
 import java.util.*;
+import java.io.*;
 
 public class createTable {
 
-    public static populateTable(String line, List<List<String>> table){
-        List<String> studentData = Arrays.asList(line.split(","));
-    }
+    // public static void populateTable(String line, List<List<String>> table){
+    //     // List<String> studentData = Arrays.asList(line.split(","));
+    //     System.out.print(line);
+    // }
 
-    public static void scanFiles(List<List<String>> table) {
-        Scanner sc = new Scanner(new File("students.txt"));
+    // public static void scanFiles(List<List<String>> table) {
+    //     Scanner sc = new Scanner(new File("students.txt"));
+    //     while (sc.hasNextLine()) {
+    //         String line = sc.nextLine();
+    //         populateTable(line, table);
+    //     }
+    // sc.close();
+    // } 
+
+    public static void scanFiles() {
+        File studentsFile = new File("students.txt");
+        System.out.println(studentsFile.canRead());
+        Scanner sc = new Scanner(studentsFile);
         while (sc.hasNextLine()) {
             String line = sc.nextLine();
-            populateTable(line, table);
+            // populateTable(line, table);
+            System.out.println(line);
         }
     sc.close();
     } 
     
     public static void main(String args[]){ 
-        List<List<String>> studentTable = new ArrayList<List<String>>();
-        scanFiles(studentTable); 
+        // List<List<String>> studentTable = new ArrayList<List<String>>();
+        // scanFiles(studentTable); 
+        scanFiles(); 
+
     }
 }
